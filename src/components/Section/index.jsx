9e449150245bdd09f5818ccc6792a0897ce5eaa1
rovedabr <rovedabr@gmail.com>
2023-02-25 09:38:25 -0300

    secction configure

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,11 +1,11 @@
 import { Container, StarsRate } from "./styles";
 import { NoteItem } from "../NoteItem";
 
-export function Section({ title, StarsRate, children, ...rest }) {
+export function Section({ title, rate, children, ...rest }) {
   return (
     <Container {...rest}>
       <h2>{title}</h2>
-      {StarsRate}
+      <p>{rate}</p>
       {children}      
     </Container>
   )
